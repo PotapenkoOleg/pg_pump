3c61e49b0ab907d46a0447a0cddc2e89683ca14d
Oleg Potapenko <PotapenkoOleg@gmail.com>
2025-12-27 20:08:26 -0500

    All types table defenition

--- sql_scripts/postgres_create.sql
+++ sql_scripts/postgres_create.sql
@@ -18,3 +18,25 @@ SELECT * FROM "Sample"."TestData1";
 SELECT COUNT(*) FROM "Sample"."TestData1";
 
 SELECT COUNT(*) FROM "Sample"."TestData1" GROUP BY "FileNumber";
+
+DROP TABLE "Sample"."AllTypes";
+
+CREATE TABLE "Sample"."AllTypes"
+(
+    "ID"         BIGINT,
+    "T_BIT"      BOOLEAN,
+    "T_BYTE"     SMALLINT,
+    "T_INT"      INT,
+    "T_LONG"     BIGINT,
+    "T_DATE"     DATE,
+    "T_TIME"     TIME,
+    "T_DATETIME" TIMESTAMP,
+    "T_DECIMAL"  DECIMAL(18, 2),
+    "T_FLOAT"    FLOAT(53),
+    "T_GUID"     UUID,
+    "T_CHAR"     CHAR(1),
+    "T_NVARCHAR" VARCHAR(255),
+    "T_VARCHAR"  VARCHAR(255)
+);
+
+SELECT * FROM "Sample"."AllTypes";
--- sql_scripts/sql_server_create.sql
+++ sql_scripts/sql_server_create.sql
@@ -13,3 +13,84 @@ CREATE TABLE [Sample].[TestData1]
 --DROP TABLE [Sample].[TestData1]
 
 SELECT count(*) FROM [Sample].[TestData1];
+
+CREATE TABLE [Sample].[AllTypes]
+(
+    ID         BIGINT,
+    T_BIT      BIT,
+    T_BYTE     SMALLINT,
+    T_INT      INT,
+    T_LONG     BIGINT,
+    T_DATE     DATE,
+    T_TIME     TIME,
+    T_DATETIME DATETIME2,
+    T_DECIMAL  DECIMAL(18, 2),
+    T_FLOAT    FLOAT(53),
+    T_GUID     UNIQUEIDENTIFIER,
+    T_CHAR     CHAR(1),
+    T_NVARCHAR NVARCHAR(255),
+    T_VARCHAR  VARCHAR(255)
+    );
+
+
+INSERT INTO [Sample].[AllTypes] (ID,
+                                 T_BIT,
+                                 T_BYTE,
+                                 T_INT,
+                                 T_LONG,
+                                 T_DATE,
+                                 T_TIME,
+                                 T_DATETIME,
+                                 T_DECIMAL,
+                                 T_FLOAT,
+                                 T_GUID,
+                                 T_CHAR,
+                                 T_NVARCHAR,
+                                 T_VARCHAR)
+VALUES (
+    1,
+    1,
+    1,
+    1,
+    1,
+    '2020-01-01',
+    '12:00:00',
+    '2020-01-01 12:00:00',
+    1.00,
+    1.00,
+    '00000000-0000-0000-0000-000000000000',
+    'A',
+    'AAA',
+    'AAA');
+
+INSERT INTO [Sample].[AllTypes] (ID,
+                                 T_BIT,
+                                 T_BYTE,
+                                 T_INT,
+                                 T_LONG,
+                                 T_DATE,
+                                 T_TIME,
+                                 T_DATETIME,
+                                 T_DECIMAL,
+                                 T_FLOAT,
+                                 T_GUID,
+                                 T_CHAR,
+                                 T_NVARCHAR,
+                                 T_VARCHAR)
+VALUES (
+    1,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL);
+
+SELECT * FROM [Sample].[AllTypes];
